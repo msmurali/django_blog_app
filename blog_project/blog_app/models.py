6532269cbfb454ab82059ba8bd72c7640f00d450
msmurali <muralikrishnan0511@gmail.com>
2022-03-17 16:38:41 +0530

    db migrations

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -1,2 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=1000)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
