9a30cad64699f4c5a80f1ba1edb6563795af659c
msmurali <muralikrishnan0511@gmail.com>
2022-03-18 14:18:18 +0530

    created users app

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -20,4 +20,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog_app.urls')),
+    path('auth/', include('users.urls')),
 ]
---
+++ blog_project/users/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+
+def register(req):
+    if(req.method == 'POST'):
+        form = UserCreationForm(req.POST)
+        if form.is_valid():
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
+    context = {'form': form}
+    return render(req, 'users/register.html', context)
